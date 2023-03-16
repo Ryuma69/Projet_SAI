a3d6a880ee17102c64bc87f09fd317ebc79484de
Ryuma69 <azedine.amirouche@gmail.com>
2023-03-16 11:27:14 +0100

    Création de la fenetre

--- src/main.c
+++ src/main.c
@@ -2,9 +2,17 @@
 #include <stdlib.h>
 #include "GL/gl.h"
 #include "GL/glut.h"
-int main()
+
+int main(int argc, char **argv)
 {
 
-    printf("Hello\n");
-    return 1;
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
+    glutInitWindowSize(800, 600);
+    glutInitWindowPosition(50, 50);
+    glutCreateWindow("Projet SAI");
+
+    glutMainLoop();
+
+    return 0;
 }
