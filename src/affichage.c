60bbfeb95fd0f36a3986cc723037a1ba844b3159
Ryuma69 <azedine.amirouche@gmail.com>
2023-03-16 11:55:47 +0100

    création des buffers pour l'animation dans la fonction affichage()

--- src/affichage.c
+++ src/affichage.c
@@ -1,1 +1,11 @@
 #include "../include/affichage.h"
+#include "GL/gl.h"
+#include "GL/glut.h"
+
+void affichage()
+{
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glutSwapBuffers();
+}
